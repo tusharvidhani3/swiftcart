699d89c8dc5a3fd5b694ab29a1e0c91f52e44697
Tushar Vidhani <tusharvidhani3@gmail.com>
2025-07-03 12:39:53 +0530

    Add endpoint to fetch all users for admin access

--- src/main/java/com/swiftcart/swiftcart/service/UserService.java
+++ src/main/java/com/swiftcart/swiftcart/service/UserService.java
@@ -1,5 +1,8 @@
 package com.swiftcart.swiftcart.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.swiftcart.swiftcart.payload.LoginRequest;
 import com.swiftcart.swiftcart.payload.RegisterRequest;
 import com.swiftcart.swiftcart.payload.UserDTO;
@@ -10,4 +13,5 @@ public interface UserService {
     public void register(RegisterRequest registerRequest);
     public UserDetailsImpl authenticate(LoginRequest loginRequest);
     public void updateUser(UserDTO userDTO);
+    public Page<UserDTO> getAllUsers(Pageable pageable);
 }
--- src/main/java/com/swiftcart/swiftcart/service/UserServiceImpl.java
+++ src/main/java/com/swiftcart/swiftcart/service/UserServiceImpl.java
@@ -1,43 +1,42 @@
 package com.swiftcart.swiftcart.service;
 
-import java.util.Set;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.swiftcart.swiftcart.entity.Role;
 import com.swiftcart.swiftcart.entity.User;
-import com.swiftcart.swiftcart.exception.BadRequestException;
 import com.swiftcart.swiftcart.payload.LoginRequest;
-import com.swiftcart.swiftcart.payload.RegisterRequest;
 import com.swiftcart.swiftcart.payload.UserDTO;
 import com.swiftcart.swiftcart.repository.RoleRepo;
 import com.swiftcart.swiftcart.repository.UserRepo;
 import com.swiftcart.swiftcart.security.UserDetailsImpl;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
-    ModelMapper modelMapper;
+    private ModelMapper modelMapper;
 
     @Autowired
-    UserRepo userRepo;
+    private UserRepo userRepo;
 
     @Autowired
-    RoleRepo roleRepo;
+    private RoleRepo roleRepo;
 
     @Autowired
-    PasswordEncoder encoder;
+    private PasswordEncoder encoder;
 
     @Autowired
-    AuthenticationManager authenticationManager;
+    private AuthenticationManager authenticationManager;
 
     @Override
     @Transactional
@@ -61,6 +60,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public void updateUser(UserDTO userDTO) {
         userRepo.save(modelMapper.map(userDTO, User.class));
+    }
+
+    @Override
+    public Page<UserDTO> getAllUsers(Pageable pageable) {
+        return userRepo.findAll(pageable).map(user -> modelMapper.map(user, UserDTO.class));
     }
 
 }
