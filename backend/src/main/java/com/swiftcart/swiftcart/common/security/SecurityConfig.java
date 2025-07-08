c4ca109cfef5dd5ee7826e4e63c4d6dbea4aaecf
Tushar Vidhani <tusharvidhani3@gmail.com>
2025-07-08 21:15:32 +0530

    chore: configure CORS for secure frontend-backend communication

--- backend/src/main/java/com/swiftcart/swiftcart/common/security/SecurityConfig.java
+++ backend/src/main/java/com/swiftcart/swiftcart/common/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -35,6 +36,7 @@ public class SecurityConfig {
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .httpBasic(httpBasic -> httpBasic.disable())
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        .cors(Customizer.withDefaults())
         .build();
     }
 
