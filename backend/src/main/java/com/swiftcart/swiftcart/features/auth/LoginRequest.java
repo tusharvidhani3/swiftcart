cbe65461edfd3e192ce48980a88e43fa03ad3b54
Tushar Vidhani <tusharvidhani3@gmail.com>
2025-09-14 11:57:00 +0530

    fix: correct mobile number validation and add password validation in LoginRequest DTO

--- backend/src/main/java/com/swiftcart/swiftcart/features/auth/LoginRequest.java
+++ backend/src/main/java/com/swiftcart/swiftcart/features/auth/LoginRequest.java
@@ -10,8 +10,10 @@ import lombok.NoArgsConstructor;
 public class LoginRequest {
 
     @NotBlank(message = "Mobile number cannot be blank")
-    @Pattern(regexp = "[6789]{1}[0-9]{9}", message = "Enter valid 10 digit mobile number")
+    @Pattern(regexp = "^[6789]{1}[0-9]{9}$", message = "Enter valid 10 digit mobile number")
     private String mobileNumber;
+
     @NotBlank(message = "Password cannot be blank")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9]).{8,}$", message = "Invalid password")
     private String password;
 }
