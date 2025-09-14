abc498bf4e8a10c54e3f0e40747d5e7cf363c1b8
Tushar Vidhani <tusharvidhani3@gmail.com>
2025-09-14 14:11:45 +0530

    feat: add sortOrder field to ProductImage entity and fetch images in sorted order

--- backend/src/main/java/com/swiftcart/swiftcart/features/product/ProductImage.java
+++ backend/src/main/java/com/swiftcart/swiftcart/features/product/ProductImage.java
@@ -6,11 +6,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = { "product_id", "sort_order" })
+})
 public class ProductImage {
 
     @Id
@@ -19,6 +24,9 @@ public class ProductImage {
 
     @Setter
     private String imageUrl;
+
+    @Setter
+    private int sortOrder;
 
     @ManyToOne
     @JoinColumn(name = "product_id")
--- backend/src/main/java/com/swiftcart/swiftcart/features/product/ProductImageRepo.java
+++ backend/src/main/java/com/swiftcart/swiftcart/features/product/ProductImageRepo.java
@@ -3,12 +3,15 @@ package com.swiftcart.swiftcart.features.product;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductImageRepo extends JpaRepository<ProductImage, Long> {
 
-    public List<ProductImage> findAllByProduct_ProductId(Long productId);
+    @Query("SELECT pi FROM ProductImage pi WHERE pi.product.productId = :productId ORDER BY pi.sortOrder")
+    public List<ProductImage> findAllByProduct_ProductId(@Param("productId") Long productId);
 
     public void deleteAllByProduct_ProductId(Long productId);
 }
