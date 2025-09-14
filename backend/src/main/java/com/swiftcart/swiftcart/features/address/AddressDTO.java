758ab59df556899e288cb75a2c5dc76b527a5dd5
Tushar Vidhani <tusharvidhani3@gmail.com>
2025-09-14 12:02:17 +0530

    refactor: update validation patterns and rules in AddressDTO and UserDTO as per standards

--- backend/src/main/java/com/swiftcart/swiftcart/features/address/AddressDTO.java
+++ backend/src/main/java/com/swiftcart/swiftcart/features/address/AddressDTO.java
@@ -12,27 +12,27 @@ public class AddressDTO {
     private Long addressId;
 
     @NotBlank(message = "Name cannot be blank")
-    @Pattern(regexp = "^[A-Za-z\\s]{3,200}$", message = "Please enter a valid name")
+    @Pattern(regexp = "^[A-Za-z]{2,50}(?:[ .'-][A-Za-z]{2,50})*$", message = "Please enter a valid name")
     private String name;
 
-    @NotBlank(message = "House no./building/apartment/company name cannot be blank")
-	@Pattern(regexp = "^[A-Za-z0-9\\s-]{3,}$", message = "Not a valid house no./building/apartment/company")
+    @NotBlank(message = "Building/Apartment/Company name cannot be blank")
+	@Pattern(regexp = "^[A-Za-z0-9\\s,./'-]{3,100}$", message = "Not a valid house no./building/apartment/company")
     private String addressLine1;
 
     @NotBlank(message = "Area/Street cannot be blank")
-	@Pattern(regexp = "^[A-Za-z\\s-]{2,}$", message = "Not a valid area/street")
+	@Pattern(regexp = "^[A-Za-z0-9\\s,./'-]{3,100}$", message = "Not a valid area/street")
     private String addressLine2;
 
     @NotBlank(message = "Pincode cannot be blank")
-	@Pattern(regexp = "^[0-9]{6}$", message = "Pincode must contain exactly 6 characters")
+	@Pattern(regexp = "^[1-9]\\d{5}$", message = "Not a valid pincode")
     private String pincode;
 
     @NotBlank(message = "City cannot be blank")
-	@Pattern(regexp = "^[A-Za-z\\s-]{2,}$", message = "Not a valid city name")
+	@Pattern(regexp = "^[A-Za-z\\s'-]{3,100}$", message = "Not a valid city name")
     private String city;
 
     @NotBlank(message = "State cannot be blank")
-	@Pattern(regexp = "^[A-Za-z\\s]{3,}$", message = "Not a valid state name")
+	@Pattern(regexp = "^[A-Za-z\\s-]{3,50}$", message = "Not a valid state name")
     private String state;
 
     @Pattern(regexp = "^[6-9]\\d{9}$", message = "Not a valid mobile number")
--- backend/src/main/java/com/swiftcart/swiftcart/features/user/UserDTO.java
+++ backend/src/main/java/com/swiftcart/swiftcart/features/user/UserDTO.java
@@ -3,6 +3,7 @@ package com.swiftcart.swiftcart.features.user;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,14 +13,16 @@ public class UserDTO {
 
     private Long userId;
 
-    @Pattern(regexp = "^[A-Za-z]+(?:[\\s.][A-Za-z]+)*$", message = "Enter valid characters in first name")
+    @Size(min = 2, max = 50, message = "First name must be 2 - 50 characters long")
+    @Pattern(regexp = "^[A-Za-z]*$", message = "Enter valid characters in first name")
     private String firstName;
 
-    @Pattern(regexp = "^[A-Za-z]+(?:[\\s.][A-Za-z]+)*$", message = "Enter valid characters in last name")
+    @Size(min = 2, max = 100, message = "Last name must be 2 - 100 characters long")
+    @Pattern(regexp = "^[A-Za-z]+(?:\\s[A-Za-z]+)*$", message = "Enter valid characters in last name")
     private String lastName;
 
     @NotBlank(message = "Please enter the mobile Number")
-    @Pattern(regexp = "[6789]{1}[0-9]{9}", message = "Enter valid 10 digit mobile number")
+    @Pattern(regexp = "^[6789]{1}[0-9]{9}$", message = "Enter valid 10 digit mobile number")
     private String mobileNumber;
 
 	@Email(message = "Please enter a valid email address")
