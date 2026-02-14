a2d9bc42b793d32eb27c0760d34176b21e98fc7b
Tushar Vidhani <tusharvidhani3@gmail.com>
2026-02-14 14:01:12 +0530

    refactor: rename LoginRequest to AuthRequest for broader usage

--- backend/src/main/java/com/swiftcart/swiftcart/features/appuser/AppUserService.java
+++ backend/src/main/java/com/swiftcart/swiftcart/features/appuser/AppUserService.java
@@ -4,12 +4,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.swiftcart.swiftcart.common.security.UserDetailsImpl;
-import com.swiftcart.swiftcart.features.auth.LoginRequest;
+import com.swiftcart.swiftcart.features.auth.AuthRequest;
 
 public interface AppUserService {
 
-    public void register(LoginRequest registerRequest);
-    public UserDetailsImpl authenticate(LoginRequest loginRequest);
+    public void register(AuthRequest registerRequest);
+    public UserDetailsImpl authenticate(AuthRequest loginRequest);
     public AppUserDto updateUser(AppUserDto userDto);
     public Page<AppUserDto> getAllUsers(Pageable pageable);
 }
